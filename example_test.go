f759378458f553d8e44ee8aaee2a820041938886
duexcoast <conor.ux@gmail.com>
2023-02-03 14:27:37 -0500

    docs and readme

---
+++ example_test.go
@@ -0,0 +1,17 @@
+package nlp_test
+
+import (
+	"fmt"
+
+	"github.com/duexcoast/nlp"
+)
+
+func ExampleTokenize() {
+	text := "Who's on first?"
+	tokens := nlp.Tokenize(text)
+
+	fmt.Println(tokens)
+
+	// Output:
+	// [who s on first]
+}
