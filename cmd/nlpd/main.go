d59ca89cd85643149087ce68cca5dd5063818859
conor.ney <conor.ux@gmail.com>
2023-02-04 02:07:11 -0500

    webserver setup

--- cmd/nlpd/main.go
+++ cmd/nlpd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// routing
+	// /health is an exact match
+	// /health/ is a prefix match
+	http.HandleFunc("/health", healthHandler)
+
+	// run server
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Run a health check/
+	fmt.Println(w, "OK")
+}
